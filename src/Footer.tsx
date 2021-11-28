c6f74b7f59190b0ec66f35079c4b5c31ddf98ed6
Brandon Truong <truong.brandon@gmail.com>
2021-11-28 13:06:35 -0800

    Create Footer.tsx

---
+++ src/Footer.tsx
@@ -0,0 +1,20 @@
+export const Footer = () => {
+    return (
+        <footer class="ph3 ph4-ns pv6 bt b--black-10 black-70">
+            <a 
+                href="mailto:mintagram@protonmail.com" 
+                class="link b f3 f2-ns dim black-70 lh-solid">
+                mintagram@protonmail.comm
+            </a>
+            <p class="f6 db b ttu lh-solid">mintagram.</p>
+            <div class="mt5">
+                <a 
+                    href="/profile/"    
+                    title="profile" 
+                    class="f6 dib ph2 mid-gray dim">
+                    profile
+                </a>
+            </div>
+        </footer>
+    )
+}
